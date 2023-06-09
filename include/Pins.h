248d150b2acc13030b8595deb6121220b938cf24
g3gg0 <git@g3gg0.de>
2023-06-09 23:42:46 +0200

    added hardware config and ARM SWD pins

--- include/Pins.h
+++ include/Pins.h
@@ -5,3 +5,16 @@
 #define DIO_SDA 21
 #define DIO_SCL 22
 #define DIO_LED 23
+
+/* from https://github.com/emporia-vue-local/esphome/discussions/173 */
+#define ATMEL_SWDIO 13
+#define ATMEL_SWCLK 14
+#define ATMEL_RST 26
+
+/* hardware config
+    0x4 - Emporia Vue 2 - "VUE Smart Home Energy Meter" - REV-4/REV-5
+*/
+#define HWCFG_0 34 0
+#define HWCFG_1 35 0
+#define HWCFG_2 32 1
+#define HWCFG_3 33 0
